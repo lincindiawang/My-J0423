4efa348fe234c43de907002ae823f70fbb5dac27
lincindiawang <lindawang0207@gmail.com>
2019-05-06 20:07:49 +0800

    0505

--- Java/src/Javaa/ScoreeeFormatException.java
+++ Java/src/Javaa/ScoreeeFormatException.java
@@ -2,4 +2,15 @@ package Javaa;
 
 public class ScoreeeFormatException {
 
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return "The type of score is wrong!!!";
+	}
+
+	public void printStackTrace() {
+		// TODO Auto-generated method stub
+		
+	}
+
 }
